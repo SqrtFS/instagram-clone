6b8d99dad60092bd9e383b6d2803a0d4442a395b
Yeldar <edil8184@gmail.com>
2026-05-16 10:25:07 +0500

    fix: update backend urls

--- frontend-sm/src/services/api.js
+++ frontend-sm/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/v1';
+const API_BASE_URL = window.location.hostname === 'localhost'
+  ? 'http://127.0.0.1:8000/v1'
+  : 'https://instagram-clone-production-50cc.up.railway.app/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- frontend-sm/vite.config.js
+++ frontend-sm/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
   server: {
     proxy: {
      '/api': {
-        target: 'https://sameway-production.up.railway.app',
+        target: 'https://instagram-clone-production-50cc.up.railway.app',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
