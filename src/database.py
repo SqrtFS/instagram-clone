c536385f43d18983a6467d2c3ec10f08c6f4d4c4
Yeldar <edil8184@gmail.com>
2026-05-16 09:18:19 +0500

    Switch to dynamic DB config and add psycopg2

--- src/database.py
+++ src/database.py
@@ -1,14 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "sqlite:///./src/db.sql"
+DB_URL = os.getenv("DATABASE_URL", "sqlite:///./src/db.sql")
 
-engine = create_engine(DB_URL , pool_pre_ping=True)
+if DB_URL.startswith("sqlite"):
+    engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+else:
+    if DB_URL.startswith("postgres://"):
+        DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+    engine = create_engine(DB_URL, pool_pre_ping=True)
 
-SessionLocal = sessionmaker(bind=engine , autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -16,3 +23,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+#
+# DB_URL = "sqlite:///./src/db.sql"
+#
+# engine = create_engine(DB_URL , pool_pre_ping=True)
+#
+# SessionLocal = sessionmaker(bind=engine , autoflush=False)
+#
+# Base = declarative_base()
+#
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
